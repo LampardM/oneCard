8255fcffeb9a4f434c775c381e258a3ce21ce2c4
zlong <lampardme@163.com>
2019-03-19 20:55:56 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="app">
-        <transition name="fade">
-            <router-view/>
-        </transition>
-    </div>
+  <div id="app">
+    <transition name="fade">
+      <router-view/>
+    </transition>
+  </div>
 </template>
 
 <script>
